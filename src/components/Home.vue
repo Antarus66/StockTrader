46a348bf0bd789d35e0ef2e9b3eb8940f95ccebf
antarus66 <antarus66@gmail.com>
2018-05-01 23:26:18 +0300

    Made a home page with hardcoded funds.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,21 @@
 <template>
-    <div>Home page</div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h1>Trade or View your portfolio</h1>
+                    <p>You may save and load your data</p>
+                    <p>Click on "End day" to begin a new day</p>
+                    <hr>
+                    <h3>Your funds: $10.000</h3>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+    export default {
+
+    }
+</script>
