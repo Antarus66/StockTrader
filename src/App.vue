fed3bc0cf64c2ac0564d386ff356c0c304355abd
antarus66 <antarus66@gmail.com>
2018-05-01 21:09:04 +0300

    Added components for header and home page.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="container">
-
+        <app-header></app-header>
+        <app-home></app-home>
     </div>
 </template>
 
 <script>
+    import Header from "./components/Header.vue";
+    import Home from "./components/Home.vue";
 
     export default {
-
+        components: {
+            appHeader: Header,
+            appHome: Home
+        }
     }
 </script>
 
