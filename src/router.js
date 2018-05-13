0757bf111a6c1168e8de83cb63595a124b4cd8f8
antarus66 <antarus66@gmail.com>
2018-05-13 21:10:02 +0300

    Moved routes to router from a separate file.

--- src/router.js
+++ src/router.js
@@ -1,10 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import routes from './routes'
+import Home from "./components/Home.vue";
+import Stocks from "./components/stocks/Stocks.vue";
+import Portfolio from "./components/portfolio/Portfolio.vue";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
     mode: "history",
-    routes
+    routes: [
+        {
+            path: "",
+            name: "home",
+            component: Home
+        },
+        {
+            path: "/stocks",
+            name: "stocks",
+            component: Stocks
+        },
+        {
+            path: "/portfolio",
+            name: "portfolio",
+            component: Portfolio
+        },
+        {
+            path: "*",
+            redirect: {
+                name: "home"
+            }
+        }
+    ]
 });
